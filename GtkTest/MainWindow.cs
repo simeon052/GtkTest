c901bafbc6bbb34ee544c6dc037db3ca0f601565
Satoshi Matsushita <simeon_the_city_who_fought@outlook.com>
2016-09-23 23:37:23 +0900

    プロジェクト ファイルを追加します。

---
+++ GtkTest/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void ClickedHandler (object sender, EventArgs e)
+	{
+        var dlg = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, $"message");
+		dlg.Show ();
+	}
+}
